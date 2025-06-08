c077638f224ba8d031c7bcd91cd6b3bab2469ec5
kalush666 <jonathan.kalush@gmail.com>
2025-06-08 22:05:44 +0300

    making mongo save json

--- Server/MongoSketchStore.cs
+++ Server/MongoSketchStore.cs
@@ -1,46 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using Shared_Models.Models;
 using PainterServer.Utils;
+using Newtonsoft.Json;
 
 namespace Server
 {
     public class MongoSketchStore
     {
-        private readonly IMongoCollection<Sketch> _collection;
+        private readonly IMongoCollection<BsonDocument> _collection;
 
-        public MongoSketchStore() {
+        public MongoSketchStore()
+        {
             var client = new MongoClient(MongoConfig.ConnectionString);
             var database = client.GetDatabase(MongoConfig.DatabaseName);
-            _collection = database.GetCollection<Sketch>(MongoConfig.CollectionName);
+            _collection = database.GetCollection<BsonDocument>(MongoConfig.CollectionName);
         }
 
         public async Task<Sketch?> GetSketchByNameAsync(string name)
         {
-            return await _collection.Find(s => s.Name == name).FirstOrDefaultAsync();
+            var filter = Builders<BsonDocument>.Filter.Eq("Name", name);
+            var document = await _collection.Find(filter).FirstOrDefaultAsync();
+            return document != null ? JsonConvert.DeserializeObject<Sketch>(document.ToJson()) : null;
         }
 
-        public async Task InsetSketchAsync(Sketch sketch) {
+        public async Task InsetSketchAsync(Sketch sketch)
+        {
             if (sketch == null || string.IsNullOrWhiteSpace(sketch.Name)) throw new ArgumentException("invalid Sketch name");
 
             var existing = await GetSketchByNameAsync(sketch.Name);
             if (existing != null) throw new InvalidOperationException($"Sketch with the name {sketch.Name} already exists");
 
-            await _collection.InsertOneAsync(sketch);
+            var json = JsonConvert.SerializeObject(sketch);
+            var document = BsonDocument.Parse(json);
+            await _collection.InsertOneAsync(document);
             Console.WriteLine($"Sketch {sketch.Name} insert to MongoDB");
         }
 
-
-        public async Task DeleteSketchAsync(string name) {
-            var found = await GetSketchByNameAsync(name);
-            if (found == null) throw new ArgumentException("invalid Sketch name");
-
-            var result = await _collection.DeleteOneAsync(s => s.Name == name);
+        public async Task DeleteSketchAsync(string name)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("Name", name);
+            var result = await _collection.DeleteOneAsync(filter);
             Console.WriteLine(result.DeletedCount > 0
                 ? $"Sketch {name} deleted from db"
                 : $"error while deleting {name} from db");
