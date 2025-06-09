a20fffc29087cab0c7905b70fcbd8fb88e19b2c2
kalush666 <jonathan.kalush@gmail.com>
2025-06-09 11:53:40 +0300

    shape factory

--- Shared Models/Factories/ShapeFactory.cs
+++ Shared Models/Factories/ShapeFactory.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shared_Models.Models;
+using Shared_Models.Enums;
 
 namespace Server.Patterns
 {
-    abstract class ShapeFactory
+    public static class ShapeFactory
     {
-        public ShapeBase Create(string type) { 
-            ShapeBase
+        public static ShapeBase Create(BasicShapeType type) { 
+            return type switch
+            {
+                BasicShapeType.Line => new Line(),
+                BasicShapeType.Rectangle => new Rectangle(),
+                BasicShapeType.Circle => new Circle(),
+                _ => throw new NotSupportedException($"{type} is not a basic type");
+            }
         }
 
 
