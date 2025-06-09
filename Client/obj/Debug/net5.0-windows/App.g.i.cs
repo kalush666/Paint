c15f9a1645b6ddc00e2d19d9f2d1d728861b11b5
kalush666 <jonathan.kalush@gmail.com>
2025-06-09 15:10:54 +0300

    mock version of server ui

--- Client/obj/Debug/net5.0-windows/App.g.i.cs
+++ Client/obj/Debug/net5.0-windows/App.g.i.cs
@@ -39,7 +39,9 @@ namespace Client {
     /// <summary>
     /// App
     /// </summary>
-    public partial class App : System.Windows.Application {
+    public partial class App : System.Windows.Application, System.Windows.Markup.IComponentConnector {
+        
+        private bool _contentLoaded;
         
         /// <summary>
         /// InitializeComponent
@@ -53,18 +55,27 @@ namespace Client {
             
             #line default
             #line hidden
+            if (_contentLoaded) {
+                return;
+            }
+            _contentLoaded = true;
+            System.Uri resourceLocater = new System.Uri("/Client;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            
+            #line 1 "..\..\..\App.xaml"
+            System.Windows.Application.LoadComponent(this, resourceLocater);
+            
+            #line default
+            #line hidden
         }
         
-        /// <summary>
-        /// Application Entry Point.
-        /// </summary>
-        [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
-        public static void Main() {
-            Client.App app = new Client.App();
-            app.InitializeComponent();
-            app.Run();
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
+        void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            this._contentLoaded = true;
         }
     }
 }
---
+++ Server/ServerWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using PainterServer.Services;
+
+namespace Server
+{
+    public partial class ServerWindow : Window
+    {
+        private TcpSketchServer _server = new();
+        private CancellationTokenSource suspendToken = new CancellationTokenSource();
+        private MongoSketchStore _mongoStore = new MongoSketchStore();
+
+        public ServerWindow()
+        {
+            InitializeComponent();
+            _server.StartListener();
+            LoadSketchList();
+        }
+
+        private async void LoadSketchList()
+        {
+            SketchList.Children.Clear();
+            var sketchFiles = await _mongoStore.GetAllJsonAsync();
+
+            foreach (var json in sketchFiles)
+            {
+                var button = new Button
+                {
+                    Content = json,
+                    Margin = new Thickness(0, 5, 0, 5),
+                    HorizontalAlignment = HorizontalAlignment.Stretch
+                };
+
+
+                SketchList.Children.Add(button);
+            }
+        }
+
+        private void SuspendButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!suspendToken.IsCancellationRequested)
+            {
+                _server.Suspend();
+                SuspendButton.Content = "Resume";
+            }
+            else
+            {
+                suspendToken = new CancellationTokenSource();
+                _server.Resume();
+                SuspendButton.Content = "Suspend";
+            }
+        }
+    }
+}
